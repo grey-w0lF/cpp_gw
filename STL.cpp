d6897fe503400fa3197586ca48e72adc4f9e9b0a
ABHINAV RAJ <abhinavrocky7488@gmail.com>
2022-11-20 01:38:55 +0530

    stlhalf

---
+++ STL.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+//     multiset<int> multier;
+    
+    
+//     // same as set but it can store duplicate items in it 
+    
+//     multier.insert(1);
+//     multier.insert(2);
+//     multier.insert(3);
+//     multier.insert(4);
+//     multier.insert(4);
+
+//     multiset <int> :: iterator itr;
+//     itr=multier.begin();
+//     while(itr!=multier.end())
+//     {
+//         cout<<*itr;
+//         itr++;
+//     }
+
+
+    
+//     // it can be used to get the count of elements in set
+//    int res= multier.count(4);
+//    cout<<res;
+
+
+    
+    return 0;
+}
---
+++ patterns.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row;
+    cin>>row;
+    int col;
+    cin>>col;
+    // for(int i=0;i<row;i++)
+    // {
+    //     for(int j=0;j<row;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=1;i<=row;i++)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=1;i<=row;i++)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=1;i<=row;i++)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         cout<<i;
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=row;i>0;i--)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    // for(int i=5;i>0;i--)
+    // {
+    //     for(int j=1;j<=i;j++)
+    //     {
+    //         cout<<j;
+    //     }
+    //     cout<<endl;
+    // }
+//    bool is = true;
+//    for(int i=1;i<=row;i++)
+//    {
+//     if(i%2!=0)
+//    {
+//     is=true;
+//    }
+//       for(int j=1;j<=i;j++)
+//       {
+//         cout<<is;
+//         is= !is;
+//       }
+//       cout<<endl;
+//    }
+    
+    return 0;
+}
